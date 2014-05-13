bd23816a05af520d46d98e5c0ae1922a8279c6d7
Daniel Griso Filho <Daniel@devappsios.com>
2014-05-13 11:14:33 -0300

    added a test class

---
+++ Git Test/iDTest.h
@@ -0,0 +1,13 @@
+//
+//  iDTest.h
+//  Git Test
+//
+//  Created by Daniel Griso Filho on 5/13/14.
+//  Copyright (c) 2014 iOS Daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iDTest : NSObject
+
+@end
---
+++ Git Test/iDTest.m
@@ -0,0 +1,13 @@
+//
+//  iDTest.m
+//  Git Test
+//
+//  Created by Daniel Griso Filho on 5/13/14.
+//  Copyright (c) 2014 iOS Daniel. All rights reserved.
+//
+
+#import "iDTest.h"
+
+@implementation iDTest
+
+@end
